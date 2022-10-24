ff1ba2bd46603a7b6a77098d05537f2a7564ec5b
Gizele Vaz <108182841+vazgizele@users.noreply.github.com>
2022-10-24 10:35:02 -0300

    commit

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -6,10 +6,9 @@ class HttpRequester:
     def __init__(self) -> None:
         self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
 
-    def request_from_page(self) -> Dict[int, str]:
+    def request_from_page(self):
         response = requests.get(self.__url)
         return {
             "status_code": response.status_code,
             "html": response.text
         }
-        
